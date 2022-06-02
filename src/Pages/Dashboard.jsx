37b270a8aa3f22b5aca457fb5d50d7a205c25c3b
Tobiloba <adedejitobiloba7@gmail.com>
2022-06-02 17:11:31 +0100

    first commit

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -16,10 +16,10 @@ const Dashboard = () => {
   return (
       <div className='board  flex flex-col relative justify-between min-w-screen min-h-screen border-black border-2 overflow-x-hidden' onLoad={blur} style={{ width: '100%', height: '100%'}}>
          <div className='container text-center h-full w-full'>
-              <div className="flex flex-col items-center flex-1 gap-14 lg:gap-4 lg:flex lg:justify-around lg:flex-row pt-6 lg:w-[1536px]">
+              <div className="flex flex-col flex-1 items-center gap-14 md:gap-4 md:flex md:justify-around md:flex-row pt-6 lg:w-[1536px]">
                   <strong className='text-4xl my-4 text-white animate-bounce transition duration-700'>Welcome User</strong>
                   <div className='pb-14'></div>
-                  <div className='lg:hidden items-center justify-center'><p className='text-2xl text-blue-600'>Organize Your Tasks On The Go...</p></div>  
+                  <div className='md:hidden items-center justify-center'><p className='text-2xl text-blue-600'>Organize Your Tasks On The Go...</p></div>  
                   <motion.div whileTap={{ scale: 0.9 }}><Link to="/signup" className="w-full lg:mr-10 p-3 text-center font-medium duration-200 bg-yellow-400 rounded-md hover:bg-yellow-200  hover:cursor-pointer">
                       Get Started
                   </Link>
